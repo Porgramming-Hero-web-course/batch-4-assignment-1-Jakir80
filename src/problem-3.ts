18ef83fbfbc969e3731fb074ada59091e6279be1
Jakir80 <jh7593617@gmail.com>
2024-11-08 16:21:17 +0600

    solved problem 3 and 8

---
+++ src/problem-3.ts
@@ -0,0 +1,10 @@
+function countWordOccurrences(text: string, searchWord: string): number {
+    return text
+        .toLowerCase()
+        .split(" ")
+        .filter(word => word === searchWord.toLowerCase())
+        .length;
+}
+
+
+// console.log(countWordOccurrences("I love typescript", "typescript"));
